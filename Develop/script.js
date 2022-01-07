b41b87153af98cc482d0a71c7ffcb444dbbf7f22
ML <labrecquemegan@hotmail.com>
2022-01-07 13:15:45 -0500

    added the concats for chosen criteria if user chooses none, all, 3 or 2 options

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,7 @@ var confirmSpecial
 var confirmUpper
 var confirmLower
 var ask
+var choices
 
 // Password variables for the conditions
 var number = [1, 2, 3, 4, 5, 6, 7, 8, 9] 
@@ -26,10 +27,39 @@ function generatePassword() {
     ask = parseInt(prompt("Please choose between 8 and 128"))
   } else {
     // ask user for their desired criteria
-    confirmNumber = confirm("Will your password contain numbers?");
-    confirmSpecial = confirm("Will your password contain special characters?");
-    confirmUpper = confirm("Will your password contain Uppercase letters?");
-    confirmLower = confirm("Will your password contain Lowercase letters?");
+    confirmNumber = confirm("Will your password contain numbers?")
+    confirmSpecial = confirm("Will your password contain special characters?")
+    confirmUpper = confirm("Will your password contain Uppercase letters?")
+    confirmLower = confirm("Will your password contain Lowercase letters?")
+  }
+
+  // This section creates the password depending on the user choices
+  // for 4 negative options
+  if (!confirmNumber %% !confirmSpecial && !confirmUpper && !confirmLower) {
+    choices = alert("You must choose at least one critera")
+  } else if (confirmNumber && confirmSpecial && confirmUpper && confirmLower) {
+    // if all answer choices chosen
+    choices = number.concat(special, letter, upper)
+  } // These are for the options of 3 criteria chosen
+    else if (confirmNumber && confirmSpecial && confirmUpper) {
+    choices = number.concat(special, upper)
+  } else if (confirmNumber && confirmSpecial && confirmLower) {
+    choices = number.concat(special, letter)
+  } else if (confirmNumber && confirmUpper && confirmLower) {
+    choices = number.concat(upper, letter)
+  } else if (confirmSpecial && confirmUpper && confirmLower) {
+    choices = special.concat(upper, letter)
+  } //for 2 criteria chosen
+    else if (confirmNumber && confirmSpecial) {
+    choices = number.concat(special)
+  } else if (confirmNumber && confirmUpper) {
+    choices = number.concat(upper)
+  } else if (confirmNumber && confirmLower) {
+    choices = number.concat(letter)
+  } else if (confirmSpecial && confirmUpper) {
+    choices = special.concat(upper)
+  } else if (confirmSpecial && confirmLower) {
+    choices = special.concat(letter)
   }
 
 
